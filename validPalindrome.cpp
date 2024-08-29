f5c7aef86b1d7b16ecf8c9cd70439c27366d98ad
Alex Chen <alexanderchen817@gmail.com>
2024-08-28 22:33:28 -0700

    Created new file

---
+++ validPalindrome.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool isPalindrome(string s) 
+{
+    bool isPalindrome = false;
+
+    // remove all non-alphanumeric characters
+    for (int i = 0; i < s.size(); i++) {
+        if (s.at(i) <= )
+    }
+
+    // convert all letters to lowercase
+    
+    // compare 
+
+    // return
+    return isPalindrome;
+}
+int main()
+{
+
+}
