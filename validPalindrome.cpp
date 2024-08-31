85bd48abebb8ac39f7701fe790fc4703d7977c26
Alex Chen <alexanderchen817@gmail.com>
2024-08-31 00:32:23 -0700

    4th try on problem :(

--- validPalindrome.cpp
+++ validPalindrome.cpp
@@ -5,30 +5,44 @@
 
 using namespace std;
 
+
+bool isNumber(char &x)
+{
+    if (x >= 48 && x <= 57) {
+        return true;
+    }
+    return false;
+}
+
+bool isAlpha(char &x)
+{
+    if ((x <= 90 && x >= 65) || (x >= 97 && x <= 122)) {
+        return true;
+    }
+    return false;
+}
+
+
 bool isPalindrome(string s) 
 {
     bool isPalindrome = false;
+    string alpha_string = "";
     // remove all non-alphanumeric characters
     for (int i = 0; i < s.size(); i++) {
-        // check for bounds of alphanumeric characters
-        // 122 is upper bound, 48 is lower bound, last two conditionals are ranges
-        if (s.at(i) > 122 || 
-            s.at(i) < 48 || (s.at(i) > 91 && s.at(i) < 97) 
-                        || (s.at(i) > 57 && s.at(i) < 65)) {
-            // erases and shortens string
-            s.erase(i);
+        if (isNumber(s.at(i)) || isAlpha(s.at(i))) {
+            alpha_string += s.at(i);
         } 
     }
 
     // convert all letters to lowercase
     for (int i = 0; i < s.size(); i++) {
-        s.at(i) = tolower(s.at(i));
+        alpha_string.at(i) = tolower(alpha_string.at(i));
     }
 
     // compare string backwards by concatentation
     string temp = "";
     for (int i = s.size() - 1; i >= 0; i--) {
-        temp += s.at(i);
+        temp += alpha_string.at(i);
     }
 
     if (temp == s) {
@@ -47,7 +61,9 @@ int main()
     // cout << isPalindrome(test) << endl;
     // string test2 = "A man, a plan, a canal: Panama";
     // cout << isPalindrome(test2) << endl;
-    string test3 = "0P";
-    cout << isPalindrome(test3) << endl;
+    // string test3 = "0P";
+    // cout << isPalindrome(test3) << endl;
+    string test4 = "a.b,.";
+    cout << isPalindrome(test4) << endl;
     return 0;
 }
