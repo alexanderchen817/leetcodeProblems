2209c97836f425dc717ec54262b44d8e5fd91a44
Alex Chen <alexanderchen817@gmail.com>
2024-09-03 00:21:00 -0700

    commit

--- palindromeLL.cpp
+++ palindromeLL.cpp
@@ -14,8 +14,6 @@ struct ListNode {
     ListNode() : val(0), next(nullptr) {}
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
-
-
 };
 
 bool isPalindrome(ListNode* head) 
---
+++ sqrt.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int mySqrt(int x)
+{
+    
+}
+int main()
+{
+    int x = 16;
+    // should print 4
+    cout << mySqrt(x) << endl;
+    return 0;
+}
