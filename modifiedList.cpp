3fd5624d685e98d6110ee02be6274fabb4b44063
Alex Chen <alexanderchen817@gmail.com>
2024-09-06 00:48:14 -0700

    Added/deleted problems

---
+++ modifiedList.cpp
@@ -0,0 +1,23 @@
+// problem #3217: Delete Nodes From Linked List Present in array
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct ListNode 
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode* modifiedList(vector<int>& nums, ListNode* head) 
+{
+    
+}
+int main()
+{
+    return 0;
+}
