c054e4e8c3cb84ceb79fddf293ed6a6d1174e891
Alex Chen <alexanderchen817@gmail.com>
2024-09-10 09:23:24 -0700

    linkedListCylce LC question in progress.

--- linkedListCycle.cpp
+++ linkedListCycle.cpp
@@ -1,3 +1,6 @@
+// Problem 141: Linked list cycle
+// Difficulty: Easy
+
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -10,6 +13,7 @@ struct ListNode {
 
 bool hasCycle(ListNode* head)
 {
+    // cycle terminates at the tail and loops back!
     // if there is only one/no element in the list, there is no cycle
     if (head->next == nullptr || head == nullptr) {
         return false;
@@ -17,14 +21,40 @@ bool hasCycle(ListNode* head)
 
     // return true if cycle, false if no cycle
     bool hasCycle = false;
-    // loop through linked list
-    while (head != nullptr) {
-        // pos is -1 if there is no cycle
+    ListNode* current = head;
+    vector<ListNode*> address_nums;
+    int counter = 0;
+    while (current != nullptr) {
+        // if tail node points to nullptr it does NOT have a cycle
+        if (current->next == nullptr) {
+            break;
+        }
 
-        // check 
+        // store the addresses in a vector
+        address_nums.push_back(current);
+        // check if address has been visited only 
+        if (address_nums.size() >= 1) {
+            for (int i = 0; i < address_nums.size(); i++) {
+                if (current->next == address_nums.at(i)) {
+                    // node has already been visited
+                    hasCycle = true;
+                }
+            }
+        }
+
+        // traverse to next node in list
+        current = current->next;
     }
+
+    return hasCycle;
 }
+
 int main()
 {
+    int x = 1;
+    int y = 2;
+    ListNode* list = new ListNode(x);
+    
+    cout << hasCycle(list) << endl;
     return 0;
 }
