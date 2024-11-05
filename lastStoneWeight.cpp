f6854ed28e42a5a1c2026b720c3f7c8fe9764d94
Alex Chen <alexanderchen817@gmail.com>
2024-11-04 18:29:48 -0800

    added lastStoneWeight leet problem

---
+++ lastStoneWeight.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <algorithm>
+using std::vector;
+using std::make_heap;
+
+int lastStoneWeight(vector<int>& stones)
+{
+    // convert stones vector into max heap 
+    make_heap(stones.begin(), stones.end());
+
+    
+}
+int main()
+{
+    return 0;
+}
