48898cef3092783eaefef6f43d2681b98d1d4419
Alex Chen <alexanderchen817@gmail.com>
2024-11-26 10:56:38 -0800

    finished searchInsertPosition

--- searchInsertPosition.cpp
+++ searchInsertPosition.cpp
@@ -3,29 +3,37 @@
 
 using std::vector;
 using std::cout;
+using std::endl;
+using std::cin;
 // must be in logn time
 // return the index
 int searchInsert(vector<int> &nums, int target)
 {
-    // use similar to binary search
-    int high = nums.size() - 1;
+    // use case is similar to binary search
+    // init variables 
+    int high = nums.size();
     int low = 0;
-    int mid = (low + nums.size()) / 2;
-    while (low <= high) {
-        if (nums.at(mid) == target) {
-            return mid;
-        } else if (nums.at(mid) < target) {
-            mid = low;
-        } else if (nums.at(mid) > target) {
-            low = mid;
-        }
-    }
+    int mid = 0;
 
-    return mid;
+    while (low < high) {
+        // must calculate mid every loop    
+        mid = (low + high) / 2;
+        if (nums.at(mid) == target) {
+            return mid; 
+        } else if (nums.at(mid) < target) {
+            low = mid + 1; // target is not at mid, is bigger 
+        } else if (nums.at(mid) > target) {
+            high = mid; // target is not at mid, is smaller
+        } 
+    }
+    // return where the index would be 
+    return low;
 }
 int main()
 {
-    vector<int> nums;
-    int target = 0;
-    cout << searchInsert(nums, target);
+    vector<int> nums {1,3,5,6};
+    int target = 2;
+    int search = searchInsert(nums, target);
+    cout << "index: " << search << endl;
+
 }
