d5f02dd9d9c187a5f4b9032daaa79cae26f5238a
Alex Chen <alexanderchen817@gmail.com>
2024-11-27 12:01:51 -0800

    finished mergedTwoSorted Lists using dummy variable

--- mergeTwoSortedLists.cpp
+++ mergeTwoSortedLists.cpp
@@ -22,61 +22,41 @@ ListNode* mergeTwoLists(ListNode* list1, ListNode* list2)
     // given heads of the 2 lists
     // return the head of the new merged list (ascending order)
 
-    // if both lists are empty
+    // base cases before looping through lists
     if (list1 == nullptr && list2 == nullptr) {
         return nullptr;
-    }
-
-    // find smaller item
-    ListNode* node1 = nullptr;
-    ListNode* node2 = nullptr;
-    if (list1 != nullptr && list2 != nullptr) {
-        if (list1->val <= list2->val) {
-            // lst 1 is smaller or equal to
-            node1 = list1;
-            node2 = list2;
-        } else {
-            // list 2 has an initial smaller value
-            node1 = list2;
-            node2 = list1;
-        }
-    } else if (list1 == nullptr && list2 != nullptr ) {
-        // list's list doesnt exist
+    } else if (list1 == nullptr && list2 != nullptr) {
         return list2;
-    } else {
-        // list2 is a nullptr so list2's list doesnt exist
+    } else if (list1 != nullptr && list2 == nullptr) {
         return list1;
     }
 
+    // uses dummy variable, and so we return the 'next' variable of the dummy since we dont wnt to return 0
+    ListNode dummy(0);
+    ListNode* tail = &dummy;
 
-    // duplicates count!
-    while (node1 != nullptr) {
-
-        if (node1->next != nullptr) {
-                ListNode* temp2 = node2->next;
-                ListNode* temp1 = node1->next;
-            if (node2->val <= node1->next->val) {
-                // ensure the bounds are met to insert between node
-                node1->next = node2; 
-                node2->next = temp1;
-
-                node2 = temp2;
-                // skip over the inserted element in node1 list
-                node1 = node1->next->next;
-            } else {
-                // traverse to the next node since we did not insert
-                node1 = node1->next;
-            }
+    while (list1 != nullptr && list2 != nullptr) {
+        if (list1->val < list2->val) {
+            // since we want the smaller value
+            tail->next = list1; // append the first element 
+            list1 = list1->next; // go to the next node in the list 
+        } else {
+            // list2's value is bigger
+            tail->next = list2;
+            list2 = list2->next;
         }
+        // traverse to next node, if nullptr loop exits
+        tail = tail->next;
     }
-    // add remaining elements 
-    if (!list2) {
-        node1->next = list2;
+
+    // append remaining nodes of either list 
+    if (list1 != nullptr){
+        tail->next = list1;
     } else {
-        node1->next = list1;
+        tail->next = list2;
     }
-    // return merged list (node1 is the merged list)
-    return node1;
+
+    return dummy.next;
 }
 int main()
 {
